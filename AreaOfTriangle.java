7721218674e799fd7327669b29017e3804224e13
Muhammad Hanan Shaikh <107320155+MuhammadHanan39@users.noreply.github.com>
2023-07-11 21:28:59 +0500

    First Files to commit from git bash

---
+++ AreaOfTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class AreaOfTriangle{
+public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+
+//int base= sc.nextInt();
+//int height = sc.nextInt();
+
+//double area= 1/2 * (base*height);
+
+float base=3;
+float height=3;
+float area =(float) (0.5*(base * height));
+
+
+System.out.println(area);
+}
+}
