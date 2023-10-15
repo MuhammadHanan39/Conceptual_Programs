ca132a1f0235b834487fd2900fef16a9d84228e9
Hanan Shaikh <hannanshaikh150@gmail.com>
2023-10-15 20:38:09 +0500

    adding some java programs

---
+++ NotOperatorOnNumbers.java
@@ -0,0 +1,14 @@
+public class NotOperatorOnNumbers{
+public static void main(String args[]){
+
+int a =10;
+
+//System.out.println(!a); this gives an error. we can only perform not operator on boolean data type
+
+boolean b=false;
+System.out.println(!b);
+
+
+
+}
+}
---
+++ ReplaceHello.java
@@ -0,0 +1,5 @@
+public class ReplaceHello{
+	public static void main(String args[] ){
+		System.out.println("hello how are you?".toUpperCase().replace("hello","bye"));
+}
+}
