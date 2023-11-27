7c898bf7697bec8dfa8a66cee023f0d3c9c3392b
Hanan Shaikh <hannanshaikh150@gmail.com>
2023-11-27 21:14:50 +0500

    Ternary Operator

---
+++ TernaryExample2.java
@@ -0,0 +1,12 @@
+public class TernaryExample2{
+public static void main(String args[]){
+
+int a =10;
+int b=20;
+
+String result= (a<b)?"A is less than b":"A is greater than b ";
+
+System.out.println(result);
+
+}
+}
